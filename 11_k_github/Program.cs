1e971571928b3b2e5f53ca4b31f6c9085c464880
zsar.daniel <zsar.daniel@C19-TANAR>
2026-02-10 08:23:45 +0100

    Add project files.

---
+++ 11_k_github/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _11_k_github
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
